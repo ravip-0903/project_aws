afc8440eda53419c37fb45aaa0e743ca19579e1b
Ravi Pandey <56596558+ravip-0903@users.noreply.github.com>
2024-12-30 21:16:16 +0530

    Update aws.php

--- aws.php
+++ aws.php
@@ -10,7 +10,7 @@
 </head>
 <body>
   <div class="wrapper">
-    <div class="title"><span> Ravi AWS fqa11 joboo   Login Form</span></div>
+    <div class="title"><span> Ravi AWS ridhi   Login Form</span></div>
     <form action="#">
       <div class="row">
         <i class="fas fa-user"></i>
